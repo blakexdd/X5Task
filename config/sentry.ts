ebdb987338bc11399a94ca98576c0f91f37820a9
Vladimir <vgololobov@stormwall.pro>
2021-06-11 13:41:54 +0300

    fix(): minor template fix

--- config/sentry.ts
+++
@@ -1,7 +0,0 @@
-import { SentryConfig } from '@ioc:Adonis/Addons/Sentry'
-import Env from '@ioc:Adonis/Core/Env'
-
-export default {
-	dsn: Env.get('SENTRY_DSN'),
-	enabled: true,
-} as SentryConfig
