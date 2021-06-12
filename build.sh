90b676890a917befd22fba961a1ad675eb245f2f
Vladimir <vgololobov@stormwall.pro>
2021-06-12 13:18:34 +0300

    fix(): minor deploy fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp .env.example .env
-docker login $CI_REPOSITORY_NAME $CI_REPOSITORY_PASSWORD 
+docker login -u $CI_REPOSITORY_NAME -p $CI_REPOSITORY_PASSWORD 
 docker build -t $APP_IMAGE -f stack/dockerfiles/Dockerfile.x5app .
 docker push $APP_IMAGE
 docker build -t $PYTHON_APP_IMAGE -f stack/dockerfiles/Dockerfile.x5python-app .
