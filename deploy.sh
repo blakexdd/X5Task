1e7b0215dd670a02ccaba9b130a1a019fe121f16
Vladimir <vgololobov@stormwall.pro>
2021-06-12 13:54:46 +0300

    feat(): sensitive data moved to env

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,16 @@ YELLOW='\033[0;33m'
 CLOSE='\033[0m'
 CYAN='\033[0;36m'
 
+read_var() {
+    VAR=$(grep "^$1" "$2" | xargs)
+    IFS="=" read -ra VAR <<<"$VAR"
+    echo "${VAR[1]}"
+}
+APP_IMAGE=$(read_var APP_IMAGE .env.docker || echo '')
+PYTHON_APP_IMAGE=$(read_var PYTHON_APP_IMAGE .env.docker || echo '')
+CI_REPOSITORY_NAME=$(read_var CI_REPOSITORY_NAME .env.docker || echo '')
+CI_REPOSITORY_PASSWORD=$(read_var CI_REPOSITORY_PASSWORD .env.docker || echo '')
+
 log(){
     echo -e "${CYAN}[$(date "+%Y-%m-%d %H:%M:%S")] $1"
 }
@@ -15,9 +25,12 @@ else
     log "${YELLOW} Network app already exists ${CLOSE}"
 fi
 
+log "${GREEN} Logging to docker repo ${CLOSE}"
+docker login -u $CI_REPOSITORY_NAME -p $CI_REPOSITORY_PASSWORD 
+
 log "${GREEN} Building images ${CLOSE}"
-docker build -t x5app -f stack/dockerfiles/Dockerfile.x5app .
-docker build -t x5python-app -f stack/dockerfiles/Dockerfile.x5python-app .
+docker build -t $APP_IMAGE -f stack/dockerfiles/Dockerfile.x5app .
+docker build -t $PYTHON_APP_IMAGE -f stack/dockerfiles/Dockerfile.x5python-app .
 
 log "${GREEN} Deploying app ${CLOSE}"
 docker stack deploy --resolve-image never -c stack/db/stack.yml app-db-stack
